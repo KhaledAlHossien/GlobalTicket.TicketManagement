9d949215ebfc49adba2ac56e3437368eec90bfd3
town-center <towncenterit@gmail.com>
2025-02-25 17:27:31 +0300

    Validation

---
+++ GlobalTicket.TicketManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace GlobalTicket.TicketManagement.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> ValidatioErrors  { get; set; }
+        public ValidationException( ValidationResult validationResult) { 
+            ValidatioErrors = new List<string>();
+
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidatioErrors.Add(validationError.ErrorMessage);
+            }
+
+        }
+    }
+}
--- GlobalTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ GlobalTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using GlobalTicket.TicketManagement.Application.Contracts.Persistence;
 
 namespace GlobalTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
     {
-        public CreateEventCommandValidator() {
+        private readonly IEventRepository _eventRepository;
+        public CreateEventCommandValidator(IEventRepository eventRepository)
+        {
+
+            _eventRepository = eventRepository;
+
             RuleFor(p => p.Name).NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull().MaximumLength(50).WithMessage("{PropertyName} must npt exceed 50 characters.");
 
@@ -18,9 +24,18 @@ namespace GlobalTicket.TicketManagement.Application.Features.Events.Commands.Cre
                 .NotNull()
                 .GreaterThan(DateTime.Now);
 
+            RuleFor(e => e)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and date already exista");
+
             RuleFor(p => p.Price)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .GreaterThan(0);
+        }
+
+        private  async Task<bool> EventNameAndDateUnique(CreateEventCommand e , CancellationToken token)
+        {
+            return !(await _eventRepository.IsEventNameAndDateUnique(e.Name,e.Date));
         }
     }
 }

10b57a9372e98bed63bc5cfc73ea7ea6d62d8153
town-center <towncenterit@gmail.com>
2025-03-05 16:54:12 +0300

    Cofiger setting to start work

---
+++ GlobalTicket.TicketManagement.Api/Program.cs
@@ -0,0 +1,10 @@
+using GlobalTicket.TicketManagement.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app=builder
+    .ConfigureServices()
+    .ConfigurePipeline();
+
+app.Run();
---
+++ GlobalTicket.TicketManagement.Api/StartupExtiensions.cs
@@ -0,0 +1,38 @@
+﻿using GlobalTicket.TicketManagement.Application;
+using GlobalTicket.TicketManagement.Infrastructure;
+using GlobalTicket.TicketManagement.Persistence;
+
+namespace GlobalTicket.TicketManagement.Api
+{
+    public static class StartupExtiensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAplicationService();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+            builder.Services.AddPersistenceServices(builder.Configuration);
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Open", builder => builder.AllowAnyOrigin
+                ().AllowAnyHeader().AllowAnyMethod());
+            });
+            return builder.Build();
+
+
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app) 
+        { 
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseCors("Open");
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
